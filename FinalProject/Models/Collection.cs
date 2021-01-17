8a0b479528ee5c2646fcd422e77b8bb360c59d28
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2021-01-17 15:43:51 +0300

    Added type files

--- FinalProject/Models/Collection.cs
+++ FinalProject/Models/Collection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FinalProject.CustomValidationAttributes;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -15,6 +16,9 @@ namespace FinalProject.Models
         public string name { get; set; } 
         public string description { get; set; }
         public string url { get; set; }
+
+        [MaxFileSize(1 * 1024 * 1024)]
+        [PermittedExtensions(new string[] { ".jpg", ".png", ".gif", ".jpeg" })]
         [NotMapped]
         public IFormFile formFile { get; set; }
         public string ImageStorageName { get; set; }
